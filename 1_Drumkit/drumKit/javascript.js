d351a643f938b0d563025200a62f8cec3816ac77
Matias Aramayo <matiasaramayo@hotmail.com>
2021-09-14 20:43:07 -0300

    finalización de js y cambios en la utilización de los sonidos

--- 1_Drumkit/drumKit/javascript.js
+++ 1_Drumkit/drumKit/javascript.js
@@ -1,9 +1,21 @@
-window.addEventListener('keydown',function(e){
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return; // Si se ejecuta una tecla que no corresponda al array de objetos en html pertenecientes a la clase data-key. Que no haga nada
     audio.currentTime = 0; // Para poder tocar repetidas veces cada nota 
     audio.play();
     key.classList.add('playing');
-})
-//Minuto 10:16 
+}
+
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return; //retorna si no es un evento transform
+    this.classList.remove('playing')
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition))
+
+
+window.addEventListener('keydown', playSound);
